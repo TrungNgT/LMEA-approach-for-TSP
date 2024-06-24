9d15e6be244ee211a012b97ca8545de7b31de541
TrungNgT <trungnguyenthanh.eworking@gmail.com>
2024-06-24 19:16:40 +0700

    commit all about LMEA

---
+++ paper_LMEA_construction/instance_pyfile.py
@@ -0,0 +1,152 @@
+# %% [markdown]
+# 1. Các thông số để lựa chọn với bộ dữ liệu
+# 
+
+# %%
+n = 14      #number of cities
+N = 16      #population 
+G = 80     #number of generations
+
+check_var = 0       # variable for checking local stuck
+
+# %%
+demension_index = 3
+start_index = 8
+graph = []
+
+# %% [markdown]
+# 2. Các định nghĩa lớp Điểm (Point) và Cá thể (Individual)
+
+# %%
+class Point:
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(x={self.x}, y={self.y})"
+    
+graph.append(Point(-1, -1))
+
+# %%
+class Individual:
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, trace, length):
+        self.trace = trace
+        self.length = length
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(trace={self.trace}, length={self.length})"
+    
+def compIndiv(indiv: Individual) :
+    return indiv.length
+
+# %% [markdown]
+# 3. Bắt đầu đọc từ file dữ liệu .txt để đưa ra danh sách (graph) các điểm
+
+# %%
+with open('C:/Users/MSI/Project2_practices/dataset/burma14.txt', 'r') as file:
+    lines = file.readlines()
+
+    demension_line = lines[demension_index]
+    n = int(demension_line[demension_line.find(' '):])
+
+    for idx_line in range(start_index, start_index+n):
+
+        cur_line = lines[idx_line]
+        component = cur_line.split(' ')
+        
+        tt = 0
+        x = 0
+        y = 0
+
+        for c in component:
+            try:
+                if tt == 0 and c != '':
+                    tt = 1
+                else:
+                    if tt == 1:
+                        x = float(c)
+                        tt = 2
+                    else:
+                        if tt == 2:
+                            y = float(c)
+                            break
+            except:
+                continue
+
+        point_idx = Point(x, y)
+        graph.append(point_idx)
+
+        del cur_line
+
+# %% [markdown]
+# 4. - Hàm tính khoảng cách theo tọa độ Euclid
+
+# %%
+import math
+
+def distance(A: Point, B: Point):
+    return math.sqrt((A.x - B.x)**2 + (A.y - B.y)**2)
+
+# %% [markdown]
+# 4. - Hàm tính khoảng cách trong trường hợp điểm được cho dữ liệu kinh độ vĩ độ
+
+# %%
+import math
+
+PI = 3.141592
+
+def GeoDistance(A: Point, B: Point):
+    # first, change degree&minute information to latitude and longtitude in radians
+    deg = round(A.x)
+    min = A.x - deg
+    latitude_A = PI * (deg + 5.0 * min / 3.0) / 180.0
+    deg = round(A.y)
+    min = A.y - deg
+    longtitude_A = PI * (deg + 5.0 * min / 3.0) / 180.0
+
+    deg = round(B.x)
+    min = B.x - deg
+    latitude_B = PI * (deg + 5.0 * min / 3.0) / 180.0
+    deg = round(B.y)
+    min = B.y - deg
+    longtitude_B = PI * (deg + 5.0 * min / 3.0) / 180.0
+
+    #compute the distance in kilometers
+    RRR = 6378.388                      #Radian of Earth
+    q1 = math.cos(longtitude_A - longtitude_B)
+    q2 = math.cos(latitude_A - latitude_B)
+    q3 = math.cos(latitude_A + latitude_B)
+
+    dist = int(RRR * math.acos(0.5 * ((1.0+q1)*q2 - (1.0-q1)*q3)) + 1.0)
+
+    return dist
+
+
+# %% [markdown]
+# 5. Biểu diễn ma trận cạnh kề, chú ý lựa chọn hàm tính khoảng cách phù hợp với bộ dữ liệu
+
+# %%
+ed_graph = []
+
+for i in range(n+1):
+    sub_grp = []
+
+    for j in range(n+1):
+        sub_grp.append(GeoDistance(graph[i], graph[j]))
+    ed_graph.append(sub_grp)
+
+# %% [markdown]
+# # Test space
+# 
+
+# %%
+#print(len(ed_graph))
+
+
---
+++ paper_LMEA_construction/support_pyfile.py
@@ -0,0 +1,168 @@
+# %%
+from instance_pyfile import *
+import random
+
+# %% [markdown]
+# File này gồm các hàm hỗ trợ cho các thao tác soạn prompt, tách kết quả từ câu trả lời của LLM và quản lý quần thể
+# 
+
+# %%
+# hàm này để đưa đồ thị danh sách điểm về liệt kê các điểm bằng string, ghép vào description của promt.
+def list2str() :
+    global graph
+    global n
+
+    out_str = ''
+    for index in range(1, n+1) :
+        out_str += (str(index) + ' (' + str(graph[index].x) + ', ' + str(graph[index].y) + ')')
+        if index != n :
+            out_str += ', '
+
+    return out_str
+
+
+# %%
+# hàm này để dịch 1 cái trace được sinh ra từ LLM (ở dạng string) sang list int (thành 1 cái permutation các chỉ số của điểm trong đồ thị)
+def traceStr2list (trace: str) :
+    global n
+    trace = trace.translate({ord('{'):None, ord('}'):None, ord(';'):',', ord('['):None, ord(']'):None, ord('\n'):None, ord('.'): ', ', ord('*'):None})
+    L = trace.split(", ")
+    for index in range(len(L)):
+        L[index] = int(L[index])
+
+    return L[:n]
+
+# %%
+# hàm này để tính chi phí đi với mỗi hoán vị được sinh ra. (Bài TSP)
+def objective_TSP(permutation: list) :                      # using the L above as the parameter.
+    evaluation = 0.0
+    x = y = 0
+    for ind in range(len(permutation) - 1) :
+        x = permutation[ind]
+        y = permutation[ind+1]
+        evaluation += ed_graph[x][y]
+        
+    evaluation += ed_graph[permutation[-1]][permutation[0]]
+
+    return evaluation
+
+# %%
+# hàm này tính chi phí bài TRP
+def objective_TRP(permutation: list) :
+    global N
+    evaluation = 0.0
+    x = y = 0
+    for ind in range(len(permutation) - 1) :
+        x = permutation[ind]
+        y = permutation[ind+1]
+        evaluation += (N-1-ind) * ed_graph[x][y]
+    
+    return evaluation
+
+# %%
+#cut response ở dạng string ra để lấy thông tin các con mới sinh ra
+def cutGenTrace(llmResponse: str):
+
+    number = min(llmResponse.count("</res>"), llmResponse.count("<res>"))
+    
+    tempString = []
+    listOffstr = []
+
+    for inte in range(number) :
+        tempString.append(str(inte))
+        listOffstr.append(str(inte))
+
+    tempString[0] = llmResponse[(llmResponse.find("<res>") + 5):]
+
+    for index in range(number) :
+        tmp = tempString[index]
+        listOffstr[index] = tmp[:tmp.find("</res>")]
+        
+        if index != number-1:
+            tempString[index+1] = tmp[(tmp.find("<res>") + 5 ):]
+
+    return listOffstr               #list of strings
+
+# %%
+#Khởi tạo quần thể ban đầu với N cá thể từ tập n điểm đã cho
+def randomFirstN(n: int, N: int):
+    listFirstN = []
+
+    while(len(listFirstN) < N) :
+        permutation =  list(random.sample(range(1, n + 1), n))
+        indiv = Individual(permutation, objective_TSP(permutation))
+        listFirstN.append(indiv)
+
+    listFirstN.sort(reverse=True, key=compIndiv)
+
+    return listFirstN
+
+# %%
+# Cập nhật quần thể sau mỗi thế hệ
+# step 6 in the paper's paradism
+def updatePool(pool: list, newgenN: list, N: int):
+
+    pool.extend(newgenN)
+    
+    pool.sort(reverse=True, key=compIndiv)
+    L = len(pool)
+
+    out_pool = pool[(L-N):]
+    pool.clear()
+
+    return out_pool
+
+# %%
+# hàm này để đưa các cá thể mẫu (trong pool đang là list các cá thể thuộc lớp Individual) thành xâu liệt kê để ghép vào phần thứ 2 của prompt
+def pool2examples(pool: list) :
+    outstr = ''
+    for index in range(4) :
+        outstr += ( str(pool[index].trace) + ' ' + 'length = ' + str(pool[index].length))
+        if index != N-1 :
+            outstr += '\n'
+    return outstr
+
+# %%
+# transform StringList offstring to IndividualList:
+
+def transform(P_temp: list):
+    P_sharp = []
+    for per in P_temp:
+        newOff = traceStr2list(per)
+        newIndiv = Individual(newOff, objective_TSP(newOff))
+        P_sharp.append(newIndiv)
+    
+    return P_sharp                  #list Individuals
+
+def checkPermu(permuStr: str, n: int) : 
+    try :            
+        permu = traceStr2list(permuStr)
+
+        if len(permu) != n:
+            return False
+    
+        else: 
+            for inte in range(1, n+1):
+                if (inte in permu) == False:
+                    return False
+    except :
+        return False
+    return True
+
+# %%
+def check_stuck(best_cur: int, best_now: int, K: int) :
+
+    global check_var
+
+    if (best_now >= best_cur) :
+        check_var += 1
+        print(f"got stuck {check_var} time")
+    else :
+        check_var = 0
+        print("got update!")
+    
+    if check_var >= K :
+        
+        return True
+    
+    return False
